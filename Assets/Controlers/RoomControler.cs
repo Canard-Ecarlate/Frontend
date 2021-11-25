c6458eaa419a77fb1e2611dc21db5e56dfaf06b7
un_phoque <unphoque38@gmail.com>
2021-11-25 10:12:12 +0100

    #37 récupération des modèles, contrôleurs, et utilitaires

---
+++ Assets/Controlers/RoomControler.cs
@@ -0,0 +1,64 @@
+﻿using CanardEcarlate.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Xml.Linq;
+
+namespace CanardEcarlate.Controlers
+{
+    class RoomControler : WebCommunicatorControler
+    {
+        public bool createRoom(string name, string nbOfPlayers)
+        {
+            try
+            {
+                string result = base.AppelWebCreateRoom(GlobalVariable.url + "room/create", name, nbOfPlayers, GlobalVariable.CurrentUser.UserId, GlobalVariable.CurrentUser.Token);
+                XNode node = JsonConvert.DeserializeXNode(result, "root");
+                XDocument doc = XDocument.Parse(node.ToString());
+                bool connection = doc.Root.Element("error") == null;
+                if (connection)
+                {
+                    GlobalVariable.CurrentRoom.name = name;
+                    return true;
+                }
+                else
+                {
+                    GlobalVariable.CurrentUser.Error = "Le nom n'est pas disponible";
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Creation error. " + e.Message + Environment.NewLine + e.InnerException);
+                GlobalVariable.CurrentUser.Error = "La salle n'a pas pu être créée";
+            }
+            return false;
+        }
+
+        public bool deleteRoom(string name)
+        {
+            try
+            {
+                string result = base.AppelWebDelete(GlobalVariable.url + "room/delete/" + name, GlobalVariable.CurrentUser.Token);
+                XNode node = JsonConvert.DeserializeXNode(result, "root");
+                XDocument doc = XDocument.Parse(node.ToString());
+                bool connection = doc.Root.Element("error") == null;
+                if (connection)
+                {
+                    return true;
+                }
+                else
+                {
+                    GlobalVariable.CurrentUser.Error = "Le salle n'a pas pu être supprimée";
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Creation error. " + e.Message + Environment.NewLine + e.InnerException);
+                GlobalVariable.CurrentUser.Error = "Le salle n'a pas pu être supprimée";
+            }
+            return false;
+        }
+    }
+}
---
+++ Assets/Models/JoinRoom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CanardEcarlate.Models
+{
+    class JoinRoom
+    {
+        public string userId { get; set; }
+        public string roomName { get; set; }
+    }
+}
