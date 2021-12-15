9208d708b1d32ca0b2bf39e37090ad48a9f7aa58
senetale <alex.senet38@gmail.com>
2021-12-15 14:38:11 +0100

    Ajout d'un handler pour l'event onClick utilisable sur tous les objets

---
+++ Assets/CustomHandlers/onClickHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace CanardEcarlate.CustomHandlers
+{
+    public class onClickHandler : MonoBehaviour, IPointerClickHandler
+    {
+        public UnityEvent onClick;
+
+        public void OnPointerClick(PointerEventData pointerEventData)
+        {
+            Debug.Log(name + " Game Object Clicked!", this);
+            onClick.Invoke();
+        }
+    }
+}
