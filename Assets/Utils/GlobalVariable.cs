e4abb97604fc473d9ac242e58d19e2720239c7be
senetale <alex.senet38@gmail.com>
2021-12-15 14:40:20 +0100

    Mise à jour de GlobalVariable pour pouvoir modifier le CurrentUser lors du login

--- Assets/Utils/GlobalVariable.cs
+++ Assets/Utils/GlobalVariable.cs
@@ -10,7 +10,7 @@ namespace CanardEcarlate.Utils
 {
     static class GlobalVariable
     {
-        public readonly static User CurrentUser = new User();
+        public static User CurrentUser = new User();
         public readonly static string url = "http://canardecarlate.fr:3100/api/";
         public readonly static HttpClient HttpClient = new HttpClient();
         public readonly static WebCommunicatorControler webCommunicatorControler = new WebCommunicatorControler();
