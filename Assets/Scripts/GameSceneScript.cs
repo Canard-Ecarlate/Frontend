b8abcfa44b3cda1a979cd20f42efa1b59ec552af
Sebastian <mrsebbymorris@gmail.com>
2022-02-01 11:15:02 +0100

    Show and hide different cards

--- Assets/Scripts/GameSceneScript.cs
+++ Assets/Scripts/GameSceneScript.cs
@@ -7,13 +7,21 @@ public class GameSceneScript : MonoBehaviour
 {
     public Button defaultCardOverlay;
     public Button myCardOverlay;
+    
     public void hideCard(Button b)
     {
         b.gameObject.SetActive(false);
     }
 
-    public void showMyCard()
+    public void showMyCard(Image i)
     {
+        myCardOverlay.gameObject.GetComponent<Image>().sprite = i.sprite;
         myCardOverlay.gameObject.SetActive(true);
+    }
+    
+    public void showACard(Image i)
+    {
+        defaultCardOverlay.gameObject.GetComponent<Image>().sprite = i.sprite;
+        defaultCardOverlay.gameObject.SetActive(true);
     }
 }
