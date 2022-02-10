6a98fad37b74c5ea71a2df51cd621d39e880838a
Sebastian <mrsebbymorris@gmail.com>
2022-02-10 10:24:31 +0100

    Finished transitions between create, private and public

--- Assets/Scripts/Script_Folder.cs
+++ Assets/Scripts/Script_Folder.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Script_Folder : MonoBehaviour
 {
+    [SerializeField] public Image createBg, privateBg, publicBg;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,26 @@ public class Script_Folder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // does nothing for now
+    }
+    
+    //Transitions
+    public void ToCreate()
+    {
+        createBg.gameObject.SetActive(true);
+        privateBg.gameObject.SetActive(false);
+        publicBg.gameObject.SetActive(false);
+    }
+    public void ToPrivate()
+    {
+        createBg.gameObject.SetActive(false);
+        privateBg.gameObject.SetActive(true);
+        publicBg.gameObject.SetActive(false);
+    }
+    public void ToPublic()
+    {
+        createBg.gameObject.SetActive(false);
+        privateBg.gameObject.SetActive(false);
+        publicBg.gameObject.SetActive(true);
     }
 }
