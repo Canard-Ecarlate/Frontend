1920ddd99eebcef983f294221a2de00801f8e748
senetale <alex.senet38@gmail.com>
2022-02-16 14:40:50 +0100

    Ajout de la méthode pour tester un login pour la connexion auto

--- Assets/Controlers/WebCommunicatorControler.cs
+++ Assets/Controlers/WebCommunicatorControler.cs
@@ -34,6 +34,13 @@ namespace CanardEcarlate.Controlers
             postData.Add(new KeyValuePair<string, string>("passwordConfirmation", passwordConfirm));
             return webCommunicator.AppelWeb(url, postData);
         }
+        
+        public string AppelWebAuthentification(string url, string token)
+        {
+            List<KeyValuePair<string, string>> postData = new List<KeyValuePair<string, string>>();
+            postData.Add(new KeyValuePair<string, string>("token", token));
+            return webCommunicator.AppelWeb(url, postData);
+        }
 
         public string AppelWebCreateRoom(string url, string name, string nbOfPlayers, string userId, string token)
         {
