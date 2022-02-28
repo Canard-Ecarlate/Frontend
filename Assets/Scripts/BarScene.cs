e0dec456adf7323f5d9ff8bd60767150bb3a0823
Sebastian <mrsebbymorris@gmail.com>
2022-02-28 11:19:33 +0100

    Set the room name and nb of players
    Removed Async
    
    
    Added sprite, added toogle for ready, added disable for play for host
    
    
    Swaps between Play and Ready button

--- Assets/Scripts/BarScene.cs
+++ Assets/Scripts/BarScene.cs
@@ -7,16 +7,127 @@ using Utils;
 
 public class BarScene : MonoBehaviour
 {
+    [SerializeField] private Text RoomName;
+    [SerializeField] private Image Player1, 
+        Player2, 
+        Player3, 
+        Player4, 
+        Player5, 
+        Player6, 
+        Player7, 
+        Player8;
+    [SerializeField] private Button PlayButton,
+        ReadyToggle;
+    private int NbPlayers = 0;
+    private string HostId, UserId;
+
     // Start is called before the first frame update
     void Start()
     {
         Screen.orientation = ScreenOrientation.Portrait;
+        HostId = GlobalVariable.Room.HostId;
+        UserId = GlobalVariable.User.Id;
+        RoomName.text = GlobalVariable.Room.Name;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // does nothing for now        
+        if (HostId == UserId)
+        {
+            ReadyToggle.gameObject.SetActive(false);
+            PlayButton.gameObject.SetActive(true);
+        }
+        if (NbPlayers == GlobalVariable.Players.Count) return;
+        NbPlayers = GlobalVariable.Players.Count;
+        SetPlayerDucks(NbPlayers);
+    }
+
+    private void SetPlayerDucks(int nbPlayers)
+    {
+        switch (nbPlayers)
+        {
+            case 1:
+                Player1.gameObject.SetActive(true);
+                Player2.gameObject.SetActive(false);
+                Player3.gameObject.SetActive(false);
+                Player4.gameObject.SetActive(false);
+                Player5.gameObject.SetActive(false);
+                Player6.gameObject.SetActive(false);
+                Player7.gameObject.SetActive(false);
+                Player8.gameObject.SetActive(false);
+                break;
+            case 2:
+                Player1.gameObject.SetActive(true);
+                Player2.gameObject.SetActive(true);
+                Player3.gameObject.SetActive(false);
+                Player4.gameObject.SetActive(false);
+                Player5.gameObject.SetActive(false);
+                Player6.gameObject.SetActive(false);
+                Player7.gameObject.SetActive(false);
+                Player8.gameObject.SetActive(false);
+                break;
+            case 3:
+                Player1.gameObject.SetActive(true);
+                Player2.gameObject.SetActive(true);
+                Player3.gameObject.SetActive(true);
+                Player4.gameObject.SetActive(false);
+                Player5.gameObject.SetActive(false);
+                Player6.gameObject.SetActive(false);
+                Player7.gameObject.SetActive(false);
+                Player8.gameObject.SetActive(false);
+                break;
+            case 4:
+                Player1.gameObject.SetActive(true);
+                Player2.gameObject.SetActive(true);
+                Player3.gameObject.SetActive(true);
+                Player4.gameObject.SetActive(true);
+                Player5.gameObject.SetActive(false);
+                Player6.gameObject.SetActive(false);
+                Player7.gameObject.SetActive(false);
+                Player8.gameObject.SetActive(false);
+                break;
+            case 5:
+                Player1.gameObject.SetActive(true);
+                Player2.gameObject.SetActive(true);
+                Player3.gameObject.SetActive(true);
+                Player4.gameObject.SetActive(true);
+                Player5.gameObject.SetActive(true);
+                Player6.gameObject.SetActive(false);
+                Player7.gameObject.SetActive(false);
+                Player8.gameObject.SetActive(false);
+                break;
+            case 6:
+                Player1.gameObject.SetActive(true);
+                Player2.gameObject.SetActive(true);
+                Player3.gameObject.SetActive(true);
+                Player4.gameObject.SetActive(true);
+                Player5.gameObject.SetActive(true);
+                Player6.gameObject.SetActive(true);
+                Player7.gameObject.SetActive(false);
+                Player8.gameObject.SetActive(false);
+                break;
+            case 7:
+                Player1.gameObject.SetActive(true);
+                Player2.gameObject.SetActive(true);
+                Player3.gameObject.SetActive(true);
+                Player4.gameObject.SetActive(true);
+                Player5.gameObject.SetActive(true);
+                Player6.gameObject.SetActive(true);
+                Player7.gameObject.SetActive(true);
+                Player8.gameObject.SetActive(false);
+                break;
+            case 8:
+                Player1.gameObject.SetActive(true);
+                Player2.gameObject.SetActive(true);
+                Player3.gameObject.SetActive(true);
+                Player4.gameObject.SetActive(true);
+                Player5.gameObject.SetActive(true);
+                Player6.gameObject.SetActive(true);
+                Player7.gameObject.SetActive(true);
+                Player8.gameObject.SetActive(true);
+                break;
+        }
     }
 
     // Beginning of Transitions section
